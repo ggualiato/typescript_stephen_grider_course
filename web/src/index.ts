40c8af8a18e53ac50972665f894d6a9eab2ed6e7
Giovanni Gualiato <ggualiato@gmail.com>
2021-10-05 23:43:41 -0300

    Testing Region Mapping

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,14 +1,16 @@
 import { User } from "./models/User";
-import { UserForm } from "./views/UserForm";
+import { UserEdit } from "./views/UserEdit";
 
 const user = User.buildUser({ name: "name", age: 20 });
 
 const root = document.getElementById("root");
 
 if (root) {
-  const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
 
-  userForm.render();
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error("root element not found");
 }
--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -3,12 +3,19 @@ import { User } from "../models/User";
 import { View } from "./View";
 
 export class UserEdit extends View<User, UserProps> {
-    template(): string {
-        return `
-            <div>
-                <div class="user-show"></div>
-                <div class="user-form"></div>
-            </div>
-        `
-    }
+  regionsMap(): { [key: string]: string } {
+    return {
+      userShow: ".user-show",
+      userForm: ".user-form",
+    };
+  }
+
+  template(): string {
+    return `
+        <div>
+            <div class="user-show"></div>
+            <div class="user-form"></div>
+        </div>
+    `;
+  }
 }
