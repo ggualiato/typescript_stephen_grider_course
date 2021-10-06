5036bbab1bd97b525bef0d574c50457a949fb2f9
Giovanni Gualiato <ggualiato@gmail.com>
2021-10-05 23:29:08 -0300

    create basic templates

---
+++ web/src/views/UserShow.ts
@@ -0,0 +1,15 @@
+import { UserProps } from "../interfaces/UserProps";
+import { User } from "../models/User";
+import { View } from "./View";
+
+export class UserShow extends View<User, UserProps> {
+    template(): string {
+        return `
+            <div>
+                <h1>User Detail</h1>
+                <div>User Name: ${this.model.get('name')}</div>
+                <div>User Age: ${this.model.get('age')}</div>
+            </div>
+        `
+    }
+}
