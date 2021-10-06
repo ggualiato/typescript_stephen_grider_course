e0a4b7fa4c46049729635e3808ee395a2f50ad0d
Giovanni Gualiato <ggualiato@gmail.com>
2021-10-05 23:51:03 -0300

    View nesting

--- web/src/views/UserEdit.ts
+++ web/src/views/UserEdit.ts
@@ -1,5 +1,7 @@
 import { UserProps } from "../interfaces/UserProps";
 import { User } from "../models/User";
+import { UserForm } from "./UserForm";
+import { UserShow } from "./UserShow";
 import { View } from "./View";
 
 export class UserEdit extends View<User, UserProps> {
@@ -8,6 +10,11 @@ export class UserEdit extends View<User, UserProps> {
       userShow: ".user-show",
       userForm: ".user-form",
     };
+  }
+
+  onRender(): void {
+    new UserForm(this.regions.userForm, this.model).render();
+    new UserShow(this.regions.userShow, this.model).render();
   }
 
   template(): string {
